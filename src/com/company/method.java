35062ab0ebb65126376d0af55c250c732f5387a5
rshek8965 <rshek8965@bths.edu>
2018-11-21 11:26:24 -0500

    fixed variable error

--- src/com/company/method.java
+++ src/com/company/method.java
@@ -15,7 +15,7 @@ public class method {
         for (int i = 0; i < list1.length; i++)
         {
             //loop from index i back to the beginning of the array, use index j
-            for (int j = i; i>0; j--)
+            for (int j = i; j>0; j--)
             {
                 if (list1[j-1]>list1[j])
                 {
--- src/com/company/runner.java
+++ src/com/company/runner.java
@@ -3,7 +3,7 @@ package com.company;
 public class runner {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2,3,4};
+        int[] arr = {1,2,3,4};
 	method.insertionSort(arr);
     }
 }
